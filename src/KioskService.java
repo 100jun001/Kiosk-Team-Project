3aed15cd551d714e86720def6ef69410ac4bcf19
Jong Hyeon Lee <ljh0990@naver.com>
2025-04-10 21:08:34 +0900

    수정중, 현재까지 충돌없음

--- src/KioskService.java
+++ src/KioskService.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 public class KioskService {
+
+    private final Scanner scanner;
+    private final Kiosk kiosk;
+    private final CartService cartService;
+
     //데이터 활용(로직 관리)
     //주석 꼭 붙여주세요
 }
