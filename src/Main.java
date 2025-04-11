7e0be449eb2331f3c568ee165d719631289006cb
100jun001 <jwcom97@naver.com>
2025-04-11 10:36:13 +0900

    브런치 테스트입니다.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // Menu 객체 생성하면서 카테고리 이름 설정
+        ArrayList<Menu> burger = new ArrayList<>();
+        ArrayList<Menu> side = new ArrayList<>();
+        ArrayList<Menu> drink = new ArrayList<>();
+
+        burger.add(new Menu("햄버거","a"));
+
         // Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입
 
         // Kiosk 객체 생성
