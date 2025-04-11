072005c1fc453f12fcb64ae2665ba1a7368ef00e
spko <spko0321@gmail.com>
2025-04-11 12:50:44 +0900

    카트서비스 test

--- src/Cart.java
+++ src/Cart.java
@@ -1,5 +1,23 @@
-public class Cart {
-    // 빈값
-    // 데이터 관리
-    // 주석 꼭 붙여주세요
-}
+import java.util.HashMap;
+    import java.util.Map;
+
+    public class Cart {
+        private Map<Product, Integer> items;
+
+        public Cart() {
+            items = new HashMap<>();
+        }
+
+        public void addItem(Product product, int quantity) {
+            items.put(product, items.getOrDefault(product, 0) + quantity);
+        }
+
+        public Map<Product, Integer> getItems() {
+            return items;
+        }
+
+        public void clear() {
+            items.clear();
+        }
+    }
+
